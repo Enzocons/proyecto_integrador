a34a4f9c0acdb2c0ce7057d1d12a229763fab827
pauuaranda <pauuaranda97@gmail.com>
2019-04-04 15:33:46 -0300

    cambios a index y inlcude navbar

--- index.php
+++ index.php
@@ -3,13 +3,17 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Hola</title>
+    <title>::BlackEye Gaming ::</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" media="screen" href="css/master.css">
+    <link rel="stylesheet" href="css/master.css">
     <script src="main.js"></script>
 </head>
 <body>
+<?php
+    include_once 'navBar.php'
+    ?>
   <div class="container-fluid p-0">
     <section class="__container-carousel">
       <div id="carouselExampleControls" class="carousel slide carousel-fade" data-ride="carousel">
