b83aa6a8a97bbd98ce7cb0b58ad4e20c701b8a09
pauuaranda <pauuaranda97@gmail.com>
2019-04-07 17:54:20 -0300

    contact link

--- navBar.php
+++ navBar.php
@@ -36,7 +36,7 @@
         </div>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Contact</a>
+        <a class="nav-link" href="contact.php">Contact</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#">FAQs</a>
