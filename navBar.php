2f0ecb7f24d9c968953c18110222785d29ebb4fe
ferrergustavo <ferrergustavo97@gmail.com>
2019-04-23 18:06:32 -0300

    cambios en el NavBar y muchas cosas mas

--- navBar.php
+++ navBar.php
@@ -2,13 +2,32 @@
 
   <div class="container-fluid p-0"> 
   <header>
-  <nav class="navbar navbar-expand-lg navbar-dark bg_negro sticky-top">
+  <nav class="navbar navbar-expand-lg navbar-dark fixed-top" style="background-color:rgb(32,32,32)">
+  <!--IMG LOGO-->
+  <div class="logo"><img src="img/logo.svg" alt=""></div>
+  <div class="botonesxs">
+      <form class="form-inline my-2 my-lg-0">
+       <!-- <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"> -->
+        <button type="submit"class="profile"><img src="img/search.png" alt=""></button>
+        <div class="dropdown">
+            <button class=" dropdown" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="profile">
+            <img src="img/profile.png" alt="">
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <a class="dropdown-item" href="login.php">Login</a>
+              <a class="dropdown-item" href="registro.php">Register</a>
+            </div>
+            </div>
+        <button type="button" class="profile2"><img src="img/carrito.png" alt=""></button>
+    </form>
+  </div>
+  <!---->
   <a class="navbar-brand" href="#" img></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
         <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
@@ -42,31 +61,24 @@
         <a class="nav-link" href="faqs.php">FAQs</a>
       </li>
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button type="submit"class="profile"><img src="img/search.png" alt=""></button>
-      <div class="dropdown">
-          <button class=" dropdown" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="profile">
-          <img src="img/profile.png" alt="">
-          </button>
-          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-            <a class="dropdown-item" href="login.php">Login</a>
-            <a class="dropdown-item" href="registro.php">Register</a>
-          </div>
-          </div>
-     <!-- <a href="login.php" class="profile" class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img src="img/profile.png" alt=""></a>
-      <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="login.php">login</a>
-          <a class="dropdown-item" href="registro.php">Register</a>
-      </div> -->
-      <button type="button" class="profile"><img src="img/carrito.png" alt=""></button>
-      
-    </form>
+    <div class="botoneslg">
+      <form class="form-inline my-2 my-lg-0">
+       <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button type="submit"class="profile"><img src="img/search.png" alt=""></button>
+        <div class="dropdown">
+            <button class=" dropdown" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="profile">
+            <img src="img/profile.png" alt="">
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <a class="dropdown-item" href="login.php">Login</a>
+              <a class="dropdown-item" href="registro.php">Register</a>
+            </div>
+            </div>
+        <button type="button" class="profile"><img src="img/carrito.png" alt=""></button>
+        
+      </form>
+    </div>
   </div>
 </nav> 
     </header>
     </div>
-<!-- <i class="fas fa-user-alt" role="button">
-<i class="fas fa-shopping-cart"></i>
-<i class="fas fa-search"></i>
--->
