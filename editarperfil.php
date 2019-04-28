acfac5f88cfdf47d0aadb443bfc8ecdcde190213
Enzocons <enzoconstantopoulos@gmail.com>
2019-04-28 16:20:43 -0300

    editar perfil responsive

--- editarperfil.php
+++ editarperfil.php
@@ -1,5 +1,5 @@
 <?php
-        require_once("controladores/funciones.php");
+    require_once("controladores/funciones.php");
 ?>
 
 <!DOCTYPE html>
@@ -16,28 +16,38 @@
 <?php
     include_once 'navBar.php'
     ?>  
-    <div class="container edit">
+    <?php /*prueba*/
+      if(isset($errores)):?>
+        <ul class="alert alert-danger">
+          <?php
+          foreach ($errores as $key => $value) :?>
+            <li> <?=$value;?> </li>
+            <?php endforeach;?>
+        </ul>
+      <?php endif;?>
+    <div class="container-fluid edit col-sm-12 col-md-9 col-lg-9 col-xl-6">
+
         <form method="POST" action="" enctype="multipart/form-data">
-        <div class="form-group edit2">
+        <div class="form-group edit2 col-sm-5 col-md-5 col-lg-5 col-xl-5">
             <label for="nombre" class="label">Nombre de perfil:</label>
-            <input name="nombre" type="text" class="form-control" id="nombre" value="">
+            <input name="nombre" type="text" class="form-control" id="nombre" value="<?= isset($_SESSION["nombre"])?  $_SESSION["nombre"] : "" ?>" >
             <div class="labelemail">
                 <label for="email" class="label">Email:</label> <!--NAME="email"-->
-                <input name="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <input name="email" type="email" class="form-control" id="exampleInputEmail1" value="<?= isset($_SESSION["email"])?  $_SESSION["email"] : "" ?>" aria-describedby="emailHelp">
             </div>
             <small id="emailHelp" class="form-text text-muted"></small>
             <label class="cambiarcontra"><a href="olvidarcontra.php">Cambiar contraseña</a></label>
             
         </div>
-        <div class="edit3">
-            <hr>
-            <label type="button" class="eliminar">Eliminar cuenta</label>
-            <label type="button" class="guardar">Guardar</label>
-            <label type="button" class="cancelar">Cancelar</label>
-        </div>
-        <div class="avataredit">
+        <div class="avataredit col-sm-5 col-md-5 col-lg-5 col-xl-5">
             <label for="avatar">Cambiar avatar<input  type="file" name="avatar" value=""/></label>
         </div>                  
+        <div class="edit3 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+            <hr>
+            <button class="eliminar">Eliminar cuenta</button>
+            <button type="submit" class="guardar">Guardar</button>
+            <button class="cancelar">Cancelar</button>
+        </div>
             </form>
         </div>
  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
