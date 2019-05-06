884c97ed7fbebd4eba83e3a0fdd9e1f8c47896f4
ferrergustavo <ferrergustavo97@gmail.com>
2019-05-06 14:03:23 -0300

    avatars nuevos

--- controladores/funciones.php
+++ controladores/funciones.php
@@ -79,12 +79,15 @@ function validar($datos,$bandera){
     }*/
     
     function crearRegistro($datos/*,$imagen*/){
-        $numero=rand(1,5);
-        if ($numero==1) { $avatarrand="../avatars/avatar1.png";}
-        if ($numero==2) { $avatarrand="../avatars/avatar2.png";}
-        if ($numero==3) { $avatarrand="../avatars/avatar3.png";}
-        if ($numero==4) { $avatarrand="../avatars/avatar4.png";}
-        if ($numero==5) { $avatarrand="../avatars/avatar5.png";}
+        $numero=rand(1,8);
+        if ($numero==1) { $avatarrand="../avatars/avatar1.svg";}
+        if ($numero==2) { $avatarrand="../avatars/avatar2.svg";}
+        if ($numero==3) { $avatarrand="../avatars/avatar3.svg";}
+        if ($numero==4) { $avatarrand="../avatars/avatar4.svg";}
+        if ($numero==5) { $avatarrand="../avatars/avatar5.svg";}
+        if ($numero==6) { $avatarrand="../avatars/avatar6.svg";}
+        if ($numero==7) { $avatarrand="../avatars/avatar7.svg";}
+        if ($numero==8) { $avatarrand="../avatars/avatar8.svg";}
         $usuario = [
             "nombre"=>$datos["nombre"],
             "email"=>$datos["email"],
